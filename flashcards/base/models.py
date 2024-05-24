fcbdd8a5e3b05b5e2cd7704faa111214a9aba016
matvey <matvey.ivanov.person@yandex.ru>
2024-05-24 14:48:44 +0700

    Added Collection and Card models

--- flashcards/base/models.py
+++ flashcards/base/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Collection(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.title[0:32]
+
+
+class Card(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    front = models.CharField(max_length=256)
+    back = models.CharField(max_length=256)
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.front[0:32]
